d924957f168d9eaf0e0be4d287689aef4fe9cf00
小馬 HI <836444147@qq.com>
2020-10-19 14:23:44 +0800

    D3D Base function and date!

---
+++ D3D_Learning/D3D_Learning/D3D_Learning.cpp
@@ -0,0 +1,12 @@
+﻿// D3D_Learning.cpp: 定义应用程序的入口点。
+//
+
+#include "D3D_Learning.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ D3D_Learning_show_your_displaycard/D3D_Learning_show_your_displaycard/D3D_Learning_show_your_displaycard.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <windows.h>
+#include "d3d12.h"
+
+typedef struct D3D12_FEATURE_DATA_FEATURE_LEVELS {
+    UINT NumFeatureLevels;
+    const D3D_FEATURE_LEVEL *pFeatureLevelsRequested;
+    D3D_FEATURE_LEVEL MaxSupportedFeatureLevel;
+};
+
+D3D_FEATURE_LEVEL featureLevels[3] = {
+    D3D_FEATURE_LEVEL_11_0,
+    D3D_FEATURE_LEVEL_10_0,
+    D3D_FEATURE_LEVEL_9_3
+};
+
+D3D12_FEATURE_DATA_FEATURE_LEVELS featureLevelsInfo;
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
