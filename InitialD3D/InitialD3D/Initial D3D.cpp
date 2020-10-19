01e1c0aae330107857c41ac364ec4caf409aa648
小馬 HI <836444147@qq.com>
2020-10-19 17:36:20 +0800

    Initial D3DApp!

--- InitialD3D/InitialD3D/Initial D3D.cpp
+++ InitialD3D/InitialD3D/Initial D3D.cpp
@@ -95,7 +95,7 @@ void InitDirect3DApp::Draw(const GameTimer& gt)
 	mCommandList->RSSetScissorRects(1, &mScissorRect);
 
 	// Clear the back buffer and depth buffer.
-	mCommandList->ClearRenderTargetView(CurrentBackBufferView(), Colors::LightSteelBlue, 0, nullptr);
+	mCommandList->ClearRenderTargetView(CurrentBackBufferView(), Colors::Black, 0, nullptr);
 	mCommandList->ClearDepthStencilView(DepthStencilView(), D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL, 1.0f, 0, 0, nullptr);
 
 	// Specify the buffers we are going to render to.
