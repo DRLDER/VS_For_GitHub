b82222eaf1f31353b882f8593c93ed0e59817dad
小馬 HI <836444147@qq.com>
2020-11-10 11:14:29 +0800

    Cs_learning_namespace

---
+++ Cs_learning_test/Cs_learning_test/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using A;
+
+
+namespace A
+{
+    class Name
+    {
+        
+        public void PrintName(string name)
+        {
+            Console.Write("My name is:[" + name + "]!");
+        }
+
+        
+    }
+    
+}
+
+namespace B
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Name nameplace=new Name();
+            Console.Write("Please write your name:\n");
+            string name = Console.ReadLine();
+            nameplace.PrintName(name);
+            Console.ReadLine();
+        }
+    }
+}
