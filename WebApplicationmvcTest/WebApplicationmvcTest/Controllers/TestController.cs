79aef49c6d83e8f658601a4daf8f0434a26b0975
小馬 HI <836444147@qq.com>
2020-12-18 09:17:30 +0800

    asp.net mvc

---
+++ WebApplicationmvcTest/WebApplicationmvcTest/Controllers/TestController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationmvcTest.Models;
+
+namespace WebApplicationmvcTest.Controllers
+{
+    public class TestController : Controller
+    {
+        //GET: Test
+        public ActionResult TestFunc(string name, int ID = 1)
+        {
+            
+            ViewBag.Message = "This is a test!"+ HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            ViewData["1"] = "This is a messageTest!";
+            ViewData["data"] = new List<modtest>()
+            {
+                new modtest
+               {
+                 ID = 1,
+                 Name = "唐僧",
+                 Age = 34,
+                 Sex = "男",
+                 Email = "747976523@qq.com"
+               },
+               new modtest
+               {
+                 ID = 2,
+                 Name = "孙悟空",
+                 Age = 635,
+                 Sex = "男",
+                 Email = "sunwukong@163.com"
+               },
+               new modtest
+               {
+                 ID = 3,
+                 Name = "白骨精",
+                 Age = 4532,
+                 Sex = "女",
+                 Email = "74345523@qq.com"
+               }
+            };
+            return View();
+        }
+       
+    }
+}
---
+++ WebApplicationmvcTest/WebApplicationmvcTest/Models/modtest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationmvcTest.Models
+{
+    public class modtest
+    {
+
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Sex { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
